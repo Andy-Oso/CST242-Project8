07bce02e74f61e8b5761ca2718048605dc9197cb
Andy <47667979+Andy-Oso@users.noreply.github.com>
2019-04-18 14:48:07 -0400

    Initial Upload

---
+++ CST242-Proejct8/CST242-Proejct8/BookInsert.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CST242_Proejct8
+{
+    public partial class BookInsert : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
